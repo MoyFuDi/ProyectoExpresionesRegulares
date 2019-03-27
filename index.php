20060bc6da9f1931db530a34fdd990eae87f2a6f
Moises Fuentes Dimas <dfuentes@univim.edu.mx>
2019-03-27 15:15:38 -0600

    Este es el commit inicial del proyecto

---
+++ index.php
@@ -0,0 +1,18 @@
+<html>
+    <head>
+        <title>Expresiones Regulares</title>
+    </head>
+    <body>
+        <h1>Validar con Expresiones Regulares</h1>
+        <p>Pagina URL:  <a href="validarUrl.php">Validar URL</a> </p>
+        <p>Pagina Email:  <a href="validarEmail.php">Validar Email</a> </p>
+        <p>Pagina Contraseña:  <a href="validarContrasena.php">Validar Contrasena</a> </p>
+        <p>Pagina Teléfono:  <a href="validarTelefono.php">Validar Telefono</a> </p>
+        <p>Pagina Tarjeta Credito:  <a href="validarTarjeta.php">Validar Tarjeta Credito</a> </p>
+        <p>Pagina ID Video:  <a href="validarIdVideo.php">Validar ID Video Youtube</a> </p>
+        <p>Pagina Etiquete Imagen:  <a href="validarEtiquetaImagen.php">Validar Etiqueta Imagen</a> </p>
+        <p>Pagina Direccion IP:  <a href="validarDireccionIP.php">Validar Direccion IP</a> </p>
+        <p>Pagina Codigo Postal:  <a href="validarCodigoPostal.php">Validar Codigo Postal</a> </p>
+        <p>Pagina Nombre Usuario:  <a href="validarNombreUsuario.php">Validar Nombre Usuario</a> </p>
+    </body>
+</html>
